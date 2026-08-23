04b92eb0bf44613818d0794313f3644d8c366408
mahadev021-dev <mahadevemitra021@gmail.com>
2026-08-23 21:33:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,14 +51,19 @@ async def handle_youtube_link(update: Update, context: ContextTypes.DEFAULT_TYPE
     out_template = os.path.join(DOWNLOAD_DIR, f"%(id)s.%(ext)s")
     
     ydl_opts = {
-        'format': 'bestaudio/best',
+        'format': 'ba/b',
         'outtmpl': out_template,
         'cookiefile': 'cookies.txt',
         'nocheckcertificate': True,
         'extractor_args': {
             'youtube': {
-                'player_client': ['web']
+                'player_client': ['android_creator', 'ios'],
+                'player_skip': ['configs', 'webpage']
             }
+        },
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9'
         },
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
